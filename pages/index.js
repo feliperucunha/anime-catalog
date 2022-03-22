41dc54dc8fa58780e7466aaed59b87fcd5bfe0ee
Felipe Cunha <felipe.cunha@compasso.com.br>
2022-03-22 09:11:53 -0300

    limpa codigo

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Link from "next/link";
 import Head from "next/head";
-import { useState, useEffect, useContext } from 'react'; 
-import { Button, Input, Typography, PageHeader, Card, Carousel } from "antd";
-import ContainerContext from "../contexts/containerContext";
+import { Card, Carousel } from "antd";
 
 const animeEndpoint = 'https://kitsu.io/api/edge/trending/anime';
 
@@ -18,46 +16,11 @@ export async function getServerSideProps() {
 }
 
 export default function IndexPage({ data }) {
-  const { searchTerm } = useContext(ContainerContext);
-  const { data: firstResults = [] } = data;
-  const [ results, setResults ] = useState(firstResults);
-  const searchEndpoint = (query) => `https://kitsu.io/api/edge/anime?filter[text]=${searchTerm}&page[limit]=20`
-
-  function handleSearchSubmit(e) {
-    e.preventDefault();
-
-    const { currentTarget = {} } = e;
-    const fields = Array.from(currentTarget?.elements);
-    const fieldQuery = fields.find(field => field.name === 'query');
-    const value = fieldQuery.value || '';
-
-    if (search) {
-      fetch(searchEndpoint(searchTerm))
-        .then(res => res.json())
-        .then(res => {
-          setResults(res.data);
-        })
-    };
-  };
-
-  function handleLandingPage() {
-    setResults(firstResults);
-  };
+  const { data: results = [] } = data;
 
   return (
     <div>
       <Head><title>Animeflix</title></Head>
-
-      {/* <PageHeader>
-        <Button onClick={handleLandingPage}>Animeflix</Button>
-        <form className="search" onSubmit={handleSearchSubmit}>
-          <Input 
-            name="query"
-            type="search"
-          />
-        </form>
-      </PageHeader> */}
-
 
       <ul className="anime-list">
         <Carousel autoplay dotPosition="bottom" slidesToShow={4}>
